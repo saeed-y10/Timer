bce86a4d3eb46cbcce4074bdaf9eabf784900288
Saeed Lahmdi <saeed_99_per@outlook.com>
2023-08-16 22:15:30 +0300

    first commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Timer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txbHours = new System.Windows.Forms.MaskedTextBox();
+            this.txbMinutes = new System.Windows.Forms.MaskedTextBox();
+            this.txbSeconds = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStopResum = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // txbHours
+            // 
+            this.txbHours.BeepOnError = true;
+            this.txbHours.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Italic);
+            this.txbHours.Location = new System.Drawing.Point(12, 238);
+            this.txbHours.Mask = "0";
+            this.txbHours.Name = "txbHours";
+            this.txbHours.Size = new System.Drawing.Size(92, 26);
+            this.txbHours.TabIndex = 0;
+            this.txbHours.Text = "0";
+            // 
+            // txbMinutes
+            // 
+            this.txbMinutes.AllowPromptAsInput = false;
+            this.txbMinutes.BeepOnError = true;
+            this.txbMinutes.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbMinutes.Location = new System.Drawing.Point(226, 238);
+            this.txbMinutes.Mask = "00";
+            this.txbMinutes.Name = "txbMinutes";
+            this.txbMinutes.Size = new System.Drawing.Size(92, 26);
+            this.txbMinutes.TabIndex = 1;
+            this.txbMinutes.Text = "00";
+            this.txbMinutes.ValidatingType = typeof(int);
+            // 
+            // txbSeconds
+            // 
+            this.txbSeconds.AllowPromptAsInput = false;
+            this.txbSeconds.BeepOnError = true;
+            this.txbSeconds.Font = new System.Drawing.Font("Comic Sans MS", 7.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbSeconds.Location = new System.Drawing.Point(440, 238);
+            this.txbSeconds.Mask = "00";
+            this.txbSeconds.Name = "txbSeconds";
+            this.txbSeconds.Size = new System.Drawing.Size(92, 26);
+            this.txbSeconds.TabIndex = 2;
+            this.txbSeconds.Text = "00";
+            this.txbSeconds.ValidatingType = typeof(int);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(7, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Hours";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(221, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Minutes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(435, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Seconds";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Inter", 9F, System.Drawing.FontStyle.Bold);
+            this.btnStart.Location = new System.Drawing.Point(12, 303);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 37);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStopResum
+            // 
+            this.btnStopResum.Font = new System.Drawing.Font("Inter", 9F, System.Drawing.FontStyle.Bold);
+            this.btnStopResum.Location = new System.Drawing.Point(226, 303);
+            this.btnStopResum.Name = "btnStopResum";
+            this.btnStopResum.Size = new System.Drawing.Size(92, 37);
+            this.btnStopResum.TabIndex = 7;
+            this.btnStopResum.Text = "Stop";
+            this.btnStopResum.UseVisualStyleBackColor = true;
+            this.btnStopResum.Click += new System.EventHandler(this.btnStopResum_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Inter", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(440, 303);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(92, 37);
+            this.btnReset.TabIndex = 8;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.Font = new System.Drawing.Font("Comic Sans MS", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDuration.ForeColor = System.Drawing.Color.Crimson;
+            this.lblDuration.Location = new System.Drawing.Point(12, 35);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(520, 72);
+            this.lblDuration.TabIndex = 9;
+            this.lblDuration.Text = "0 : 0 : 0";
+            this.lblDuration.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 147);
+            this.progressBar1.MarqueeAnimationSpeed = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(520, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 10;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Timer Complete";
+            this.notifyIcon1.BalloonTipTitle = "Allert";
+            this.notifyIcon1.Text = "Timer Complete";
+            this.notifyIcon1.Visible = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 376);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lblDuration);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStopResum);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txbSeconds);
+            this.Controls.Add(this.txbMinutes);
+            this.Controls.Add(this.txbHours);
+            this.Name = "MainForm";
+            this.Text = "Timer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MaskedTextBox txbHours;
+        private System.Windows.Forms.MaskedTextBox txbMinutes;
+        private System.Windows.Forms.MaskedTextBox txbSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStopResum;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private TimeSpan TotalTime;
+        private TimeSpan DurationTime;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.Icon = SystemIcons.Application;
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipTitle = "Allert";
+            notifyIcon1.BalloonTipText = "Timer complete";
+        }
+
+        private void EnableTimer()
+        {
+            timer1.Enabled = true;
+        }
+
+        private void DisableTimer()
+        {
+            timer1.Enabled = false;
+        }
+
+        private void EnableTxtBoxs()
+        {
+            txbHours.Enabled = true;
+            txbMinutes.Enabled = true;
+            txbSeconds.Enabled = true;
+        }
+
+        private void EnableStartButton()
+        {
+            btnStart.Enabled = true;
+        }
+
+        private void DisableStartButton()
+        {
+            btnStart.Enabled = false;
+        }
+
+
+        private void EnableStopResumButton()
+        {
+            btnStopResum.Enabled = true;
+        }
+
+        private void DisableStopResumButton()
+        {
+            btnStopResum.Enabled = false;
+        }
+
+        private void DisableTxtBoxs()
+        {
+            txbHours.Enabled = false;
+            txbMinutes.Enabled = false;
+            txbSeconds.Enabled = false;
+        }
+
+        private void ResetAllDefaults()
+        {
+            DisableTimer();
+            EnableTxtBoxs();
+            EnableStartButton();
+            EnableStopResumButton();
+
+            lblDuration.Text = "0 : 0 : 0";
+            txbHours.Text = "0";
+            txbMinutes.Text = "00";
+            txbSeconds.Text = "00";
+
+            TotalTime = new TimeSpan(0, 0, 0);
+            DurationTime = new TimeSpan(0, 0, 0);
+            progressBar1.Value = 0;
+        }
+
+        private void UpdateDurationLabel()
+        {
+            lblDuration.Text = DurationTime.Hours.ToString() + " : " + DurationTime.Minutes.ToString() + " : " + DurationTime.Seconds.ToString();
+        }
+
+        private bool IsAllInputsAreZero()
+        {
+            return (Convert.ToInt32(txbHours.Text) == 0 && Convert.ToInt32(txbMinutes.Text) == 0 && Convert.ToInt32(txbSeconds.Text) == 0);
+        }
+
+        private bool IsValidInputs()
+        {
+            return (Convert.ToInt32(txbHours.Text) >= 0 && Convert.ToInt32(txbMinutes.Text) >= 0 && Convert.ToInt32(txbSeconds.Text) >= 0);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (IsAllInputsAreZero())
+            {
+                ResetAllDefaults();
+                return;
+            }
+
+            if (txbHours.MaskCompleted && txbMinutes.MaskCompleted && txbSeconds.MaskCompleted)
+            {
+
+                TotalTime = new TimeSpan(Convert.ToInt32(txbHours.Text), Convert.ToInt32(txbMinutes.Text), Convert.ToInt32(txbSeconds.Text));
+                DurationTime = new TimeSpan(0, 0, 0);
+
+                progressBar1.Maximum = Convert.ToInt32(TotalTime.TotalSeconds);
+
+                DisableStartButton();
+                DisableTxtBoxs();
+                EnableTimer();
+            }
+
+            else
+            {
+                MessageBox.Show("invaled inputs", "error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (DurationTime.TotalSeconds < TotalTime.TotalSeconds && progressBar1.Value < progressBar1.Maximum)
+            {
+                DurationTime = DurationTime.Add(new TimeSpan(0, 0, 1));
+                progressBar1.Value += 1;
+
+                UpdateDurationLabel();
+
+                //progressBar1.Refresh();
+                //lblDuration.Refresh();
+            }
+
+            else
+            {
+                DisableTimer();
+                DisableStopResumButton();
+                notifyIcon1.ShowBalloonTip(30000);
+            }
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetAllDefaults();
+        }
+
+        private void btnStopResum_Click(object sender, EventArgs e)
+        {
+            if (btnStopResum.Text == "Stop")
+            {
+                btnStopResum.Text = "Resum";
+                DisableTimer();
+            }
+
+            else
+            {
+                btnStopResum.Text = "Stop";
+                EnableTimer();
+            }
+        }
+
+        private void notifyIcon1_BalloonTipShown(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
